ba4e4566f0a236f8e2a44bdab3528877d4e3bae8
AnshVM <anshmalik2002@gmail.com>
2021-08-30 13:51:55 +0530

    .

--- server.js
+++ server.js
@@ -10,30 +10,30 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const path = require('path');
 
-app.use('/api',routes)
+app.use('/api', routes)
 
 dotenv.config();
 mongoose.set('bufferCommands', false);
 
-await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true})
 
 const port = process.env.PORT || 5000;
 
-io.on('connection',(socket)=>{
-    socket.on('POLL_UPDATE',(data)=>{
-        io.emit('POLL_UPDATE',data);
-        socket.broadcast.emit('POLL_UPDATE',data);
+io.on('connection', (socket) => {
+    socket.on('POLL_UPDATE', (data) => {
+        io.emit('POLL_UPDATE', data);
+        socket.broadcast.emit('POLL_UPDATE', data);
     })
 })
 
 
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.get("*", (req, res) => {
+app.get("*", async (req, res) => {
+    await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-http.listen(port,()=>{
+http.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
 
